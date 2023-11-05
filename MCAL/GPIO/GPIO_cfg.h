c2798f14097918553a9080a58e6e08943b685e59
Abdalah Beshary <abdalahbesho@gmail.com>
2023-11-06 01:55:42 +0200

    this Gpio Driver

---
+++ MCAL/GPIO/GPIO_cfg.h
@@ -0,0 +1,15 @@
+/*
+ * GPIO_cfg.h
+ *
+ *  Created on: Aug 22, 2022
+ *      Author: DELL
+ */
+
+#ifndef MCAL_GPIO_GPIO_CFG_H_
+#define MCAL_GPIO_GPIO_CFG_H_
+
+
+
+
+
+#endif /* MCAL_GPIO_GPIO_CFG_H_ */
