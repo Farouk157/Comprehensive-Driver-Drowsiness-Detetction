b5dd13fdeb59b0c5fffedf1700f035a551772d22
Ashraf <mohamed.ashraf99.scurt@gmail.com>
2023-11-06 00:41:25 +0200

    Check NVIC Driver

---
+++ MCAL/NVIC/NVIC_config.h
@@ -0,0 +1,16 @@
+/*******************************************************/
+/*      Author  :   Mohamed Ashraf Mohamed                    */
+/*      SWC     :   NVIC                               */
+/*      Date    :   7/10/2023                           */
+/*      version :       1.00                           */
+/*******************************************************/
+
+
+#ifndef MCAL_NVIC_NVIC_CONFIG_H_
+#define MCAL_NVIC_NVIC_CONFIG_H_
+
+
+
+
+
+#endif /* MCAL_NVIC_NVIC_CONFIG_H_ */
