a6367ad5217c9fa22e01e5ee25498a267ef55015
farouk <faroukehap90@gmail.com>
2023-11-09 04:34:24 +0200

    This is the UART driver

---
+++ MCAL/UART/UART_cfg.c
@@ -0,0 +1,52 @@
+/*
+ * UART_cfg.c
+ *
+ *  Created on: Aug 30, 2023
+ *      Author: admin
+ */
+
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
+
+#include "UART_cfg.h"
+#include "UART_int.h"
+/*
+ * BaudRate
+ * Bluetooth    9600
+ * WIFI         115200
+ */
+USART_InitType uart1_cfg =
+{
+		.BaudRate           = 115200,
+		.DataWidth          = USART_MODE_8BIT,
+		.StopBits           = USART_STOP_BIT_1,
+		.Parity_Enable      = DISABLE,
+		.Parity_Selection   = USART_EVEN_PARITY,
+		.TransferDirection  = USART_TX_RX,
+		.Oversampling       = USART_OVER_SAMPLING_8
+
+};
+USART_InitType uart2_cfg =
+{
+		.BaudRate           = 115200,
+		.DataWidth          = USART_MODE_8BIT,
+		.StopBits           = USART_STOP_BIT_1,
+		.Parity_Enable      = DISABLE,
+		.Parity_Selection   = USART_EVEN_PARITY,
+		.TransferDirection  = USART_TX_RX,
+		.Oversampling       = USART_OVER_SAMPLING_8
+
+};
+USART_InitType uart6_cfg =
+{
+		.BaudRate           = 115200,
+		.DataWidth          = USART_MODE_8BIT,
+		.StopBits           = USART_STOP_BIT_1,
+		.Parity_Enable      = DISABLE,
+		.Parity_Selection   = USART_EVEN_PARITY,
+		.TransferDirection  = USART_TX_RX,
+		.Oversampling       = USART_OVER_SAMPLING_8
+
+};
+
+
